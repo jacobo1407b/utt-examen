ce7f2a093755b48d592408808ddcdb7d3d5fabc5
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2020-06-22 17:47:15 -0500

    Actualizando menu
    Se trata de hacer responsivo el menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -13,17 +13,28 @@ const Menu = (props) => {
     return (
         <div>
             <nav>
-                <div className="nav-wrapper green">
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li>{cerrar ? (
-                            <a href="#!" className="btn-floating btn-large halfway-fab waves-effect waves-light red" onClick={logout} title="cerrar sesión">
-                                <i className="material-icons">exit_to_app</i>
-                            </a>
-                        ) : <Exit exitExam={null} />}
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+                <div className="nav-wrapper  green darken-3">
+                        <a href="#!" className="brand-logo">SEANI</a>
+                        <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <ul className="right hide-on-med-and-down">
+                            <li>{cerrar ? (
+                                <a href="#!" className="btn-floating btn-large halfway-fab waves-effect waves-light red" onClick={logout} title="cerrar sesión">
+                                    <i className="material-icons">exit_to_app</i>
+                                </a>
+                                ) : <Exit exitExam={null} />}
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <ul className="sidenav" id="mobile-demo">
+                    <li>{cerrar ? (
+                                <a href="#!" className="btn-floating btn-large halfway-fab waves-effect waves-light red" onClick={logout} title="cerrar sesión">
+                                    <i className="material-icons">exit_to_app</i>
+                                </a>
+                                ) : <Exit exitExam={null} />}
+                            </li>
+                </ul>
         </div>
     )
 }
